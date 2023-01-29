ba2ff371d750fee9c6bbe3698af4b5159f81cfcc
iMadfxq <imadfxq@gmail.com>
2023-01-28 21:08:59 -0500

    Started to create real hero

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -5,8 +5,17 @@ import fall from '../../public/Fulmer_Falls_Closeup_3000px.jpg'
 import building from '../../public/mohsen-ameri-eX5BI7h-PGQ-unsplash.jpg'
 
 export default function Hero() {
-  return <>
-  <Image src={fall} width='300' height={'240'} placeholder='blur'></Image>
-  <Image src={building} width='300' height={'240'} placeholder='blur'  ></Image>
-  </>
+  return (
+    <section>
+      <div>
+        <Image></Image>
+      </div>
+      <div>
+        <div>
+          steps
+        </div>
+        <h1>My journey as a human</h1>
+      </div>
+    </section>
+  )
 }
